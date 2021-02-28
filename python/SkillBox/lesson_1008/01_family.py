853417bfad5a15dfe4f582c6d953bfada3fc2bbb
Elena Mozhenkova <Elena.Mozhenkova@gmail.com>
2021-02-28 23:17:05 +0300

    lesson_1008 (add baby)

--- python/SkillBox/lesson_1008/01_family.py
+++ python/SkillBox/lesson_1008/01_family.py
@@ -178,90 +178,46 @@ class Wife(Man):
 
         print(f'{self.name} уборка квартиры: fullness {self.fullness} dirt {self.house.dirt}')
 
+class Child(Man):
+
+    def __init__(self, name, house):
+        self.happiness = 100
+        self.fullness = 10
+        self.house = house
+        self.name = name
+
+    def __str__(self):
+        return super().__str__()
+
+    def act(self):
+        if self.fullness <= 10:
+            self.eat()
+        else:
+            self.sleep()
+
+    def eat(self):
+        self.fullness += 10
+        print(f'{self.name} покушал: fullness {self.fullness} ')
+
+    def sleep(self):
+        self.fullness -= 5
+        print(f'{self.name} поспал: fullness {self.fullness} ')
+
 
 home = House()
 serge = Husband(name='Сережа', house=home)
 masha = Wife(name='Маша', house=home)
+baby = Child(name='Саша', house=home)
 
 for day in range(365):
     cprint('================== День {} =================='.format(day), color='red')
     home.add_dirt()
     serge.act()
     masha.act()
+    baby.act()
     cprint(serge, color='cyan')
     cprint(masha, color='cyan')
+    cprint(baby, color='cyan')
     cprint(home, color='cyan')
 
 
-# TODO после реализации первой части - отдать на проверку учителю
-
-######################################################## Часть вторая
-#
-# После подтверждения учителем первой части надо
-# отщепить ветку develop и в ней начать добавлять котов в модель семьи
-#
-# Кот может:
-#   есть,
-#   спать,
-#   драть обои
-#
-# Люди могут:
-#   гладить кота (растет степень счастья на 5 пунктов)
-#
-# В доме добавляется:
-#   еда для кота (в начале - 30)
-#
-# У кота есть имя и степень сытости (в начале - 30)
-# Любое действие кота, кроме "есть", приводит к уменьшению степени сытости на 10 пунктов
-# Еда для кота покупается за деньги: за 10 денег 10 еды.
-# Кушает кот максимум по 10 единиц еды, степень сытости растет на 2 пункта за 1 пункт еды.
-# Степень сытости не должна падать ниже 0, иначе кот умрет от голода.
-#
-# Если кот дерет обои, то грязи становится больше на 5 пунктов
-
-
-class Cat:
-
-    def __init__(self):
-        pass
-
-    def act(self):
-        pass
-
-    def eat(self):
-        pass
-
-    def sleep(self):
-        pass
-
-    def soil(self):
-        pass
-
-
-######################################################## Часть вторая бис
-#
-# После реализации первой части надо в ветке мастер продолжить работу над семьей - добавить ребенка
-#
-# Ребенок может:
-#   есть,
-#   спать,
-#
-# отличия от взрослых - кушает максимум 10 единиц еды,
-# степень счастья  - не меняется, всегда ==100 ;)
-
-class Child:
-
-    def __init__(self):
-        pass
-
-    def __str__(self):
-        return super().__str__()
-
-    def act(self):
-        pass
-
-    def eat(self):
-        pass
-
-    def sleep(self):
-        pass
